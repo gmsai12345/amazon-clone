e7e126218494da8f1676c67bf48ac04c42c1c429
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-29 07:39:28 +0530

    Update Footer.jsx

--- amazonclone1/src/Footer/Footer.jsx
+++ amazonclone1/src/Footer/Footer.jsx
@@ -1,36 +1,104 @@
 import React from "react";
 export default function () {
-  return ( 
-  <div>
-    <span>
-    <p>See personalized recommendations</p>
-    </span>
-    <span>
-    <button type = "submit">Sign in</button>
-    </span>
-    <span>
-      <p>New customer?</p>
-      <p>Start here</p>
-    </span>
-    <span>
-      <button type = "submit"> Back to top</button>
-    </span>
-   
-    <span>
-      <p>Get to know us</p>
-    </span>
-    <span>
-      <p>About us</p>
-    </span>
-    <span>
-      <p>Careers</p>
-    </span>
-    <span>
-      <p>Press Release</p>
-    </span>
-    <span>
-      <p>Amazon Science</p>
-    </span>
+  return (
+    <div>
+      <span>
+        <p>See personalized recommendations</p>
+      </span>
+      <span>
+        <button type="submit">Sign in</button>
+      </span>
+      <span>
+        <p>New customer?</p>
+        <p>Start here</p>
+      </span>
+      <span>
+        <button type="submit"> Back to top</button>
+      </span>
+
+      <div>
+        <span>
+          <p>Get to know us</p>
+        </span>
+        <span>
+          <p>About us</p>
+        </span>
+        <span>
+          <p>Careers</p>
+        </span>
+        <span>
+          <p>Press Release</p>
+        </span>
+        <span>
+          <p>Amazon Science</p>
+        </span>
+      </div>
+      <div>
+        <span>
+          <p>Connect with Us</p>
+        </span>
+        <span>
+          <p>Facebook</p>
+        </span>
+        <span>
+          <p>Twitter</p>
+        </span>
+        <span>
+          <p>Instagram</p>
+        </span>
+      </div>
+      <div>
+        <span>
+          <p>Make Money with Us</p>
+        </span>
+        <span>
+          <p>Sell on Amazon</p>
+        </span>
+        <span>
+          <p>Sell under Amazon Accelerator</p>
+        </span>
+        <span>
+          <p>Amazon Global Selling</p>
+        </span>
+        <span>
+          <p>Become an Affiliate</p>
+        </span>
+        <span>
+          <p>Fulfilment by Amazon</p>
+        </span>
+        <span>
+          <p>Advertise Your Products</p>
+        </span>
+        <span>
+          <p>Amazon Pay on Merchants</p>
+        </span>
+      </div>
+      <div>
+        <span>
+          <p>Let Us Help You</p>
+        </span>
+        <span>
+          <p>COVID-19 and Amazon</p>
+        </span>
+        <span>
+          <p>Your Account</p>
+        </span>
+        <span>
+          <p>Returns Centre</p>
+        </span>
+        <span>
+          <p>100% Purchase Protection</p>
+        </span>
+        <span>
+          <p>Amazon App Download</p>
+        </span>
+        <span>
+          <p>Amazon Assistant Download</p>
+        </span>
+        <span>
+          <p>Help</p>
+        </span>
+      </div>
     </div>
   );
 }
