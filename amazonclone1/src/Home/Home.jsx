9e0d8f503c19a4cc08700ddc187299faeb0f9db2
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-30 09:42:04 +0530

    Update Home.jsx

--- amazonclone1/src/Home/Home.jsx
+++ amazonclone1/src/Home/Home.jsx
@@ -4,36 +4,47 @@ export default function () {
   return (
     <div>
       <p>Redefine your living room</p>
+      <div>
       <img
         src=""
         alt=""
         onClick="https://www.amazon.in/l/27060486031?pf_rd_r=73TBDRX335YNQX73WDRN&pf_rd_p=022afc4b-77d5-49ee-81b9-639e0f7a250f&pd_rd_r=9e79206c-33c3-410d-aeb0-45ad10b95bab&pd_rd_w=yPwl6&pd_rd_wg=rlH23&ref_=pd_gw_unk"
       />
+      <span>Sofa cum beds</span></div>
+      <div>
       <img
         src=""
         alt=""
         onClick="https://www.amazon.in/l/27060486031?pf_rd_r=73TBDRX335YNQX73WDRN&pf_rd_p=022afc4b-77d5-49ee-81b9-639e0f7a250f&pd_rd_r=9e79206c-33c3-410d-aeb0-45ad10b95bab&pd_rd_w=yPwl6&pd_rd_wg=rlH23&ref_=pd_gw_unk"
       />
+      <span>Office chairs and study tables</span></div>
+
+      <div>
       <img
         src=""
         alt=""
         onClick="https://www.amazon.in/l/27060486031?pf_rd_r=73TBDRX335YNQX73WDRN&pf_rd_p=022afc4b-77d5-49ee-81b9-639e0f7a250f&pd_rd_r=9e79206c-33c3-410d-aeb0-45ad10b95bab&pd_rd_w=yPwl6&pd_rd_wg=rlH23&ref_=pd_gw_unk"
       />
+      <span>Bean bags</span></div>
+      <div>
       <img
         src=""
         alt=""
         onClick="https://www.amazon.in/l/27060486031?pf_rd_r=73TBDRX335YNQX73WDRN&pf_rd_p=022afc4b-77d5-49ee-81b9-639e0f7a250f&pd_rd_r=9e79206c-33c3-410d-aeb0-45ad10b95bab&pd_rd_w=yPwl6&pd_rd_wg=rlH23&ref_=pd_gw_unk"
       />
+      <span>Explore All</span></div>
       <span>
         <p className="bottom-image"> Visit our furniture store</p>
       </span>
       <span>
         <p>Revamp your home in style</p>
+        <div>
         <img
           src=""
           alt=""
           onClick="https://www.amazon.in/l/27060486031?pf_rd_r=73TBDRX335YNQX73WDRN&pf_rd_p=022afc4b-77d5-49ee-81b9-639e0f7a250f&pd_rd_r=9e79206c-33c3-410d-aeb0-45ad10b95bab&pd_rd_w=yPwl6&pd_rd_wg=rlH23&ref_=pd_gw_unk"
         />
+        <span>Smart LED TVs Up to 45% off | Smart LED TVs</span></di>
         <img
           src=""
           alt=""
