c2a87556205d6d328971eb66225d396cb3a78c71
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-30 20:23:18 +0530

    Update Homredirect.jsx

--- amazonclone1/src/Homredirect.jsx
+++ amazonclone1/src/Homredirect.jsx
@@ -1,4 +1,4 @@
-=import React from "react";
+import React from "react";
 import Navbar1 from "./Navbar1/Navbar1";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -40,6 +40,13 @@ export default function ()
                   <div>
                   <span>Delivery Day </span>
                     </div>
+                    <div>
+                    <p>Delivery Day</p>
+                      <span><input type="radio" id="child" name="age" value="child"/>
+<label for="child">Get it by Tommorow</label>
+<input type="radio" id="adult" name="age" value="adult"/>
+<label for="adult">Get it in 2 days</label></span>
+                      </div>
     </Navbar>
     </Nav>
     </div>
